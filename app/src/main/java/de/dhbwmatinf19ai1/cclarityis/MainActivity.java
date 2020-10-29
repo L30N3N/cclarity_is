71d5962043d4ce84e1917e1141f8a8c6088b130a
YannickS99 <schroth.yannick@gmail.com>
2020-10-29 14:48:27 +0100

    Json auf Knopfdruck

---
+++ app/src/main/java/de/dhbwmatinf19ai1/cclarityis/MainActivity.java
@@ -0,0 +1,66 @@
+package de.dhbwmatinf19ai1.cclarityis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    String url;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        url = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=1%3D1&outFields=Landkreis,AnzahlFall&outSR=4326&f=json";
+        btn = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                OkHttpClient client = new OkHttpClient();
+
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            final String myResponse = response.body().string();
+
+                            MainActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    textView.setText(myResponse);
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
